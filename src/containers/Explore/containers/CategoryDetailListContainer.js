216f33f765c23ed9ede2c57ec35a1f1ef17ff6c5
psi9730 <psi9730@naver.com>
2020-08-08 15:13:41 +0900

    implement inifinite scroll hooks

--- src/containers/Explore/containers/CategoryDetailListContainer.js
+++ src/containers/Explore/containers/CategoryDetailListContainer.js
@@ -9,10 +9,17 @@ import React, {
 import { useParams, useHistory, useLocation } from "react-router-dom";
 import styled, { css } from "styled-components";
 import request from "/utils/request";
-import { Box, Flex, Text } from "/components/Common";
+import { Box, Flex, Text, ImgNoSrc } from "/components/Common";
 import { CategoryDetailCard } from "/components/StyleCard";
 import { RestAPIContext } from "stores";
-import useIntersectionObserver from "hooks/useIntersectionObserver";
+import useIntersect from "hooks/useIntersectionObserver";
+
+const LoadingContainer = styled(Box)`
+    padding: 10px 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 const Container = styled(Box)`
     width: 100%;
@@ -28,88 +35,126 @@ const CategoryDetailListContainer = ({ id }) => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [datas, setDatas] = useState([]);
-
-    const [currentDatas, setCurrentDatas] = useState([]);
+    const [loadMore, setLoadMore] = useState(true);
     const offset = useRef(0);
     const rootRef = useRef(null);
     const targetRef = useRef(null);
 
-    const limit = 4;
-    const loadData = useCallback(async () => {
-        try {
-            setLoading(true);
-            const fetchData = await RestAPI(
-                "GET_LIST",
-                `categories/` + id + "/articles",
-                {
-                    id,
-                    pagination: {
-                        offset: offset.current,
-                        limit: limit,
-                    },
-                    sort: { field: "created_at", order: "desc" },
-                }
-            ).then((res) => res.data);
-            setCurrentDatas(fetchData);
-            return fetchData;
-        } catch (e) {
-            setError(e);
-            setLoading(false);
-        } finally {
-            setLoading(false);
+    const limit = 2;
+    const onIntersect = useCallback(async (entry, observer, id, loadMore) => {
+        if (loadMore) {
+            observer.unobserve(entry.target);
+            await loadMoreData(id);
+            observer.observe(entry.target);
         }
-    }, [RestAPI, id]);
-    const loadMoreData = useCallback(async () => {
-        if (datas.length > 0) {
-            offset.current = offset.current + limit;
-            const data = await loadData();
-            data && data.length > 0 && setDatas([...datas, ...data]);
-        }
-    }, [datas, loadData]);
-
-    useIntersectionObserver({
-        root: rootRef.current,
-        target: targetRef.current,
-        onIntersect: ([{ isIntersecting }]) => {
-            if (isIntersecting && !!offset.current && currentDatas.length > 0) {
-                loadMoreData();
-            }
-        },
     });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await loadData();
-            if (data) {
-                setDatas(data);
-            } else {
-                setDatas([]);
-            }
-        };
+    const [_, setRef] = useIntersect(onIntersect, id, loadMore);
 
-        fetchData();
-    }, [loadData]);
-    return datas.length > 0 ? (
-        <Flex flexDirection="column" mt="24px" ml="24px" mr="24px">
-            {datas.map((_data) => (
-                <CategoryDetailCard
-                    css={`
-                        margin-bottom: 24px;
-                    `}
-                    key={_data.id}
-                    data={{
-                        id: _data.id,
-                        tags: _data.tags,
-                        title: _data.title,
-                        original_article_url: _data.original_article_url,
-                        image_url: _data.square_image_url,
-                        // title: _data.title,
-                        // summary: _data.summary,
-                    }}
-                />
-            ))}
-        </Flex>
-    ) : null;
+    const loadMoreData = useCallback(
+        async (id) => {
+            const data = await loadData(id);
+            if (data && data.length > 0) {
+                setDatas((datas) => datas.concat(data));
+                offset.current = offset.current + limit;
+            } else setLoadMore(false);
+        },
+        [loadData, offset, id]
+    );
+
+    const loadData = useCallback(
+        async (id) => {
+            try {
+                setLoading(true);
+
+                const fetchData = await RestAPI(
+                    "GET_LIST",
+                    `categories/` + id + "/articles",
+                    {
+                        id,
+                        pagination: {
+                            offset: offset.current,
+                            limit: limit,
+                        },
+                        sort: { field: "created_at", order: "desc" },
+                    }
+                ).then((res) => res.data);
+                return fetchData;
+            } catch (e) {
+                setError(e);
+                setLoading(false);
+            } finally {
+                setLoading(false);
+            }
+        },
+        [RestAPI]
+    );
+
+    // useIntersectionObserver({
+    //     root: rootRef.current,
+    //     target: targetRef.current,
+    //     onIntersect: ([{ isIntersecting }]) => {
+    //         console.log(
+    //             isIntersecting,
+    //             datas,
+    //             offset.current,
+    //             currentDatas.length
+    //         );
+    //         if (
+    //             isIntersecting &&
+    //             offset.current !== undefined &&
+    //             currentDatas.length > 0
+    //         ) {
+    //             loadMoreData();
+    //         }
+    //     },
+    // });
+
+    useEffect(() => {
+        offset.current = 0;
+        return () => {
+            setDatas([]);
+            setLoadMore(true);
+        };
+    }, [id]);
+
+    if (datas === undefined || error) {
+        return null;
+    }
+    return (
+        <Box>
+            <Flex
+                ref={rootRef}
+                flexDirection="column"
+                mt="24px"
+                ml="24px"
+                mr="24px"
+            >
+                {datas.map((_data) => (
+                    <CategoryDetailCard
+                        css={`
+                            margin-bottom: 24px;
+                        `}
+                        key={_data.id}
+                        data={{
+                            id: _data.id,
+                            tags: _data.tags,
+                            title: _data.title,
+                            original_article_url: _data.original_article_url,
+                            image_url: _data.square_image_url,
+                        }}
+                    />
+                ))}
+            </Flex>
+            <div ref={setRef}>
+                <LoadingContainer>
+                    {loading && (
+                        <ImgNoSrc src="loader.gif" width={30} height={30} />
+                    )}
+                </LoadingContainer>
+            </div>
+        </Box>
+    );
 };
 
 export default CategoryDetailListContainer;
--- src/hooks/useIntersectionObserver.js
+++ src/hooks/useIntersectionObserver.js
@@ -1,32 +1,65 @@
-import { useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
-export default ({
-    root,
-    target,
-    onIntersect,
-    threshold = 1.0,
-    rootMargin = "0px",
-}) => {
-    useEffect(() => {
-        if (!root) {
-            return;
-        }
-
-        const observer = new IntersectionObserver(onIntersect, {
-            root,
-            rootMargin,
-            threshold,
-        });
-
-        if (!target) {
-            return;
-        }
-
-        observer.observe(target);
-
-        // Let's clean up after ourselves.
-        return () => {
-            observer.unobserve(target);
-        };
-    }, [target, root, rootMargin, onIntersect, threshold]);
+const baseOption = {
+    root: null,
+    threshold: 0.5,
+    rootMargin: "0px",
 };
+const useIntersect = (onIntersect, id, loadMore, option = baseOption) => {
+    const [ref, setRef] = useState(null);
+    const checkIntersect = useCallback(
+        ([entry], observer) => {
+            if (entry.isIntersecting) {
+                onIntersect(entry, observer, id, loadMore);
+            }
+        },
+        [id, loadMore]
+    );
+    useEffect(() => {
+        let observer;
+        if (ref) {
+            observer = new IntersectionObserver(checkIntersect, {
+                ...option,
+            });
+            observer.observe(ref);
+        }
+        return () => {
+            observer && observer.disconnect();
+        };
+    }, [ref, option.root, option.threshold, option.rootMargin, checkIntersect]);
+    return [ref, setRef];
+};
+
+export default useIntersect;
+
+// export default ({
+//     root,
+//     target,
+//     onIntersect,
+//     threshold = 1.0,
+//     rootMargin = "0px",
+// }) => {
+//     useEffect(() => {
+//         if (!root) {
+//             return;
+//         }
+
+//         const observer = new IntersectionObserver(onIntersect, {
+//             root,
+//             rootMargin,
+//             threshold,
+//         });
+
+//         if (!target) {
+//             return;
+//         }
+
+//         observer.observe(target);
+
+//         // Let's clean up after ourselves.
+//         return () => {
+//             console.log("href unobserve");
+//             observer.unobserve(target);
+//         };
+//     }, [target, root, rootMargin, onIntersect, threshold]);
+// };
